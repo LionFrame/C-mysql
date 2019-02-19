42b22d01659b0fdea36e4dd85f1cd30d1313fe89
Dinar Hamadiev <hamadiev.root@gmail.com>
2019-02-19 23:50:07 +0300

    MysqlCOnnection1

---
+++ MySqlBase/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace MySqlBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                string connection = "server=localhost;user=root;database=user;";
+                MySqlConnection resourseMysql = new MySqlConnection(connection);
+                resourseMysql.Open();
+
+                //string sqlCommand = "SHOW TABLES FROM user;";
+                string sqlCommand = "DESCRIBE customers";
+                MySqlCommand selectDescribe = new MySqlCommand(sqlCommand, resourseMysql);
+                //Console.WriteLine(selectDescribe);
+                MySqlDataReader reader = selectDescribe.ExecuteReader();
+
+                //Console.WriteLine(reader.FieldCount);
+
+
+                while (reader.Read())
+                {
+
+                    int i = 0;
+                    string result = "";
+                    for (; i < reader.FieldCount; i++)
+                    {
+                        
+                        result += $"{reader[i].ToString()} ";
+                    };
+                    //Console.WriteLine($"{reader[0].ToString()} ");
+                    Console.WriteLine(result);
+                    
+                }
+                reader.Close(); 
+
+                resourseMysql.Close();
+                Console.ReadKey();
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
